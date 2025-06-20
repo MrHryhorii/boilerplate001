694c1f721f230aab665dfd88537ca35925853114
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-06-20 12:50:25 +0200

    random-color-onClick

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+let divItems = document.getElementsByClassName("small-box");
+const defaultColor = 'aquamarine'
+
+function selected(item) {
+    //this.clear();
+    item.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function clear() {
+    for(var i=0; i < divItems.length; i++) {
+        var item = divItems[i];
+        item.style.backgroundColor = 'white';
+    }
+}
