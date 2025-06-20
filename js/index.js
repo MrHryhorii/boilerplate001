894cf0eab43fdfd3286b18b29956fb5fa27d45cc
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-06-20 12:52:19 +0200

    fix-clear

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ let divItems = document.getElementsByClassName("small-box");
 const defaultColor = 'aquamarine'
 
 function selected(item) {
-    //this.clear();
+    //this.clear(); //reset rest colors
     item.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 function clear() {
     for(var i=0; i < divItems.length; i++) {
         var item = divItems[i];
-        item.style.backgroundColor = 'white';
+        item.style.backgroundColor = defaultColor;
     }
 }
